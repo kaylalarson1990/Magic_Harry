a3e54694fe0576a55517986f32d95b68849b7f18
Kayla <kayla.larson1990@gmail.com>
2019-07-25 17:51:51 -0600

    Add styling to characters page and search bar hover styling

--- src/Characters/Characters.jsx
+++ src/Characters/Characters.jsx
@@ -1,36 +1,44 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import './Characters.css'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import inactive from "../images/inactive.png";
+import active from "../images/active.png";
+import "./Characters.css";
 
 class Characters extends Component {
-    constructor() {
-        super()
-        this.state = {
-            error: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      favorites: false,
+      error: ""
+    };
+  }
 
-    render(){
-        const {name, role, house, school, id, species} = this.props
-        return(
-            <article className="characterCard">
-                <h2>{name}</h2>
-                <p>{role}</p>
+  render() {
+    const { name, role, house, school, id, species } = this.props;
+    return (
+      <article className="characterCard">
+        <h2>{name}</h2>
+        <p>Click for more details</p>
+        {/* <p>{role}</p>
                 <p>{house}</p>
                 <p>{school}</p>
-                <p>{species}</p>
-            </article>
-
-        )
-    }
+                <p>{species}</p> */}
+        <img
+          src={this.state.favorited ? active : inactive}
+          alt="favorite icon"
+          className="favorite-btn"
+        />
+      </article>
+    );
+  }
 }
 
 Characters.propTypes = {
-    name: PropTypes.string,
-    role: PropTypes.string,
-    house: PropTypes.string,
-    species: PropTypes.string,
-    id: PropTypes.number
-}
+  name: PropTypes.string,
+  role: PropTypes.string,
+  house: PropTypes.string,
+  species: PropTypes.string,
+  id: PropTypes.number
+};
 
 export default Characters;
