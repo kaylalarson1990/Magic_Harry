af675042861535329836baf834a67a984edb995d
Kayla <kayla.larson1990@gmail.com>
2019-07-25 17:14:16 -0600

    Add details to homepage

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import "./Header.css";
-// import MischiefManaged from "../images/MischiefManaged.png";
+import glasses from "../images/glasses.jpg";
 import { NavLink } from "react-router-dom";
-
 
 export const Header = () => {
   return (
     <header>
       <nav>
         <ul className="nav">
-          <li><a href="#"><NavLink to='/'>WIZARDLY WORLD</NavLink></a></li>
-          <li><a href="#"><NavLink to="/characters">CHARACTERS</NavLink></a></li>
-          <li><a href="#"><NavLink to="/spells">SPELLS</NavLink></a></li>
-          <li><a href="#"><NavLink to="/houses">HOUSES</NavLink></a></li>
+          <li>
+            <a href="#" className="glasses">
+              <NavLink to="/">
+                <img
+                  src={glasses}
+                  alt="harry potter glasses"
+                  className="glasses"
+                />
+              </NavLink>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <NavLink to="/characters">CHARACTERS</NavLink>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <NavLink to="/spells">SPELLS</NavLink>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <NavLink to="/houses">HOUSES</NavLink>
+            </a>
+          </li>
         </ul>
       </nav>
     </header>
